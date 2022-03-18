dc525bec652d1bcd60535e560443c7a0aeb6eab2
Harsh Mangalam <hmangalam@oplglobal.com>
2022-03-18 18:53:52 +0530

    activity indicator

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import ExpoImage from "./src/screens/ExpoImage";
+// import ExpoImage from "./src/screens/ExpoImage";
+import Indicator from "./src/screens/Indicator";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <ExpoImage />
+      <Indicator />
     </View>
   );
 }
---
+++ src/screens/Indicator.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ActivityIndicator, View } from "react-native";
+
+const App = () => (
+  <View>
+    <ActivityIndicator size="small" color="#0000ff" />
+    <ActivityIndicator size="large" color="#00ff00" />
+  </View>
+);
+
+export default App;
